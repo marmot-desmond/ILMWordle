66b7b97ba4af84c90c4d280ad216816eaff5e001
marmot-desmond <koltonlindell@gmail.com>
2026-06-13 18:17:17 -0400

    typo

--- scripts/supabaseClient.js
+++ scripts/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://yfujkwiqjlverofmqgrr.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_HuM5CtDyqJh9O6RCNbKnsw_sUy-5gBS";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_HuM5CtDyqJh9O6RCNbKnsw_sUy-5gBS";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
